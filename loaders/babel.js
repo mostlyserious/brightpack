73293e4dba8b9f55f0de24ab7ec83b10fae4397a
Cornelius Ukena <guy@cornelius.dev>
2018-12-20 14:17:01 -0600

    invalidate babel cache when browserslist changes

--- loaders/babel.js
+++ loaders/babel.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const babel = require('@babel/core');
+const browserslist = require('browserslist');
+const loader = require('babel-loader/package.json');
+const json = JSON.stringify;
 
 module.exports = {
     test: /\.([tj]sx?|svelte|svlt)$/,
@@ -6,7 +10,8 @@ module.exports = {
         {
             loader: 'babel-loader',
             options: {
-                cacheDirectory: path.resolve('.cache/js')
+                cacheDirectory: path.resolve('.cache/js'),
+                cacheIdentifier: babel.version + loader.version + json(babel.loadOptions()) + json(browserslist.findConfig(process.cwd()))
             }
         }
     ]
