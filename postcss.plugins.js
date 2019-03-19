089642a78da9709cb81859f167baf29c17f2218b
Cornelius Ukena <guy@cornelius.dev>
2019-03-19 18:29:00 -0500

    update core-js to 3, update tailwind config filename

--- postcss.plugins.js
+++ postcss.plugins.js
@@ -5,7 +5,7 @@ const postcssImport = require('postcss-import');
 const postcssNested = require('postcss-nested');
 const requireOptional = require('./lib/require-optional');
 const postcssColorFunction = require('postcss-color-function');
-const tailwindConfig = path.resolve(process.cwd(), 'tailwind.js');
+const tailwindConfig = path.resolve(process.cwd(), 'tailwind.config.js');
 const tailwindcss = requireOptional('tailwindcss');
 
 module.exports = [
