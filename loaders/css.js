1926dd1e4ac58ef8ba71dfbda23dde76d8f1cb81
Cornelius Ukena <guy@cornelius.dev>
2020-02-10 15:26:30 -0600

    dep update

--- loaders/css.js
+++ loaders/css.js
@@ -3,7 +3,12 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     test: /\.(post)?css$/,
     use: [
-        global.inProduction ? MiniCssExtractPlugin.loader : {
+        global.inProduction ? {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                esModule: true
+            }
+        } : {
             loader: 'style-loader'
         },
         {
