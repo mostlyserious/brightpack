1643599c24f8861a200cdbd6edd49be28c062bff
Cornelius Ukena <guy@cornelius.dev>
2020-09-17 18:55:56 -0500

    🐛 fix wrongly name active class

--- tailwindcss/is-active.js
+++ tailwindcss/is-active.js
@@ -4,8 +4,8 @@ module.exports = plugin(({ addVariant, e }) => {
     addVariant('is-active', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
             return `
-                .is-active .${e(`active${separator}${className}`)},
-                .${e(`active${separator}${className}`)}.is-active
+                .is-active .${e(`is-active${separator}${className}`)},
+                .${e(`is-active${separator}${className}`)}.is-active
             `;
         });
     });
