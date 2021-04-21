fbe78b4f7f918a799334445664c06359d0f09ce9
Cornelius Ukena <guy@cornelius.dev>
2021-04-21 10:07:54 -0500

    🔥 cleanup

--- loaders/vue.js
+++ loaders/vue.js
@@ -1,17 +1,8 @@
-const path = require('path');
-const cache = require('cache-loader/package.json');
-const requireOptional = require('../lib/require-optional');
-const vue = requireOptional('vue') || { version: 0 };
-
 module.exports = {
     test: /\.vue$/,
     use: [
         {
-            loader: 'vue-loader',
-            options: {
-                cacheDirectory: path.resolve('.cache/vue'),
-                cacheIdentifier: `cache-loader:${cache.version} vue:${vue.version}`
-            }
+            loader: 'vue-loader'
         }
     ]
 };
