8f69ad91c7fa4abfea92dafacb16270adfc3d3af
Cornelius Ukena <guy@cornelius.dev>
2022-07-08 09:57:26 -0500

    🐛 update is-active definition

--- tailwindcss/is-active.js
+++ tailwindcss/is-active.js
@@ -1,10 +1,5 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(({ addVariant, e }) => {
-    addVariant('is-active', ({ modifySelectors, separator }) => {
-        modifySelectors(({ className }) => [
-            `.is-active .${e(`is-active${separator}${className}`)},`,
-            `.${e(`is-active${separator}${className}`)}.is-active`
-        ].join('\n'));
-    });
+    addVariant('is-active', [ '.is-active &', '&.is-active' ]);
 });
